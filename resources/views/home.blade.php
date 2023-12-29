5ef267da729bd17fcf2baa04b481b969ccc27fca
Michael <Michaelpage05@hotmail.co.uk>
2023-12-29 09:37:55 +0000

    basic layout
    changed lando project name and set up a basic blade layout

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,13 +1,3 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-
-        <title>{{ config('app.name') }}</title>
-        @vite('resources/css/app.css')
-    </head>
-    <body>
-        <h1 class="text-3xl font-bold underline">Hello World!</h1>
-    </body>
-</html>
+<x-layout>
+    <h1 class="text-3xl font-bold underline">Hello World!</h1>
+</x-layout>
