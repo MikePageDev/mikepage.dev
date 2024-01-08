cd81f74701c276dfc9ebaf97ac23455aa606001d
Michael <Michaelpage05@hotmail.co.uk>
2024-01-08 07:53:10 +0000

    menu bar

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -8,6 +8,13 @@
     @vite('resources/css/app.css')
 </head>
 <body>
- {{ $slot }}
+    <nav class="flex h-20">
+        <a href="{{ route('home') }}" class="h-14 w-14 rounded-full border-2 border-white group shadow">
+            <img src="{{ asset('img/mike.jpg') }}" alt="Mike Page" class="rounded-full group-hover:opacity-80 transition-all duration-150">
+        </a>
+    </nav>
+    <main>
+        {{ $slot }}
+    </main>
 </body>
 </html>
